a07b06ea53b0df28fbd75a4769e71a92b42c6c19
Nur Fatin Amirah Mohd Samsudin <nurfatinamirahmohdsamsudin@Nurs-MacBook-Air.local>
2025-06-11 12:48:38 +0800

    Fix: set correct Django settings module

--- tabbycat/wsgi.py
+++ tabbycat/wsgi.py
@@ -1,9 +1,8 @@
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 # Needed for WSGI Callable
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tabbycat.settings')
 
 # Needed for waitress/gunicorn serving
 application = get_wsgi_application()
